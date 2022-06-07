fe5497a87c2cdd9a52a4afcc5ddd76840d6d1404
kevin-luna <kevin.luna200344@gmail.com>
2022-06-07 02:05:55 -0500

    Se agregaron setters, getters y un constructor vacío a la clase Cargo.

--- src/com/itver/Cargo.java
+++ src/com/itver/Cargo.java
@@ -5,6 +5,8 @@ public class Cargo {
     private String descripcion;
     private float precio;
 
+    public Cargo(){}
+
     public Cargo(String concepto, float precio) {
         this.concepto = concepto;
         this.precio = precio;
@@ -16,5 +18,27 @@ public class Cargo {
         this.precio = precio;
     }
 
+    public String getConcepto() {
+        return concepto;
+    }
 
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
 }
