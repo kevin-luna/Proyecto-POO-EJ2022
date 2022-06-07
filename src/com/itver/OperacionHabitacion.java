e8334cfe2b9c3ac3090fe91d1e067862f5db1265
kevin-luna <kevin.luna200344@gmail.com>
2022-06-07 03:25:34 -0500

    Leer y guardar ahora son métodos abstractos de OperacionHabitacion.

--- src/com/itver/OperacionHabitacion.java
+++ src/com/itver/OperacionHabitacion.java
@@ -1,15 +1,21 @@
 package com.itver;
 
+import java.io.BufferedReader;
+
 public abstract class OperacionHabitacion {
     private Cliente cliente;
     private int habitacion;
 
-    public OperacionHabitacion(){};
+    public OperacionHabitacion(){}
 
     public OperacionHabitacion(Cliente cliente, int habitacion) {
         this.cliente = cliente;
         this.habitacion = habitacion;
     }
+
+    public abstract void leer(BufferedReader bfr);
+
+    public abstract void guardar(String file);
 
     public Cliente getCliente() {
         return cliente;
