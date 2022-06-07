902f1437cce417c25328df12d809209632aaa2ee
kevin-luna <kevin.luna200344@gmail.com>
2022-06-07 04:08:08 -0500

    Factura cuenta con métodos leer y guardar.

--- src/com/itver/Factura.java
+++ src/com/itver/Factura.java
@@ -1,19 +1,60 @@
 package com.itver;
 
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Factura {
-    final private Cliente cliente;
-    final private Cargo[] cargos;
-    final private float total;
+    private Cliente cliente;
+    private Cargo[] cargos;
+    private float total;
+
+    public Factura(){}
 
     public Factura(Cliente cliente, Cargo[] cargos, float total) {
         this.cliente = cliente;
         this.cargos = cargos;
         this.total = total;
+    }
+
+    public void leer(BufferedReader bfr){
+        int cargos=0;
+        do{
+            try {
+                System.out.println("Ingrese el RFC del cliente:");
+                String rfcCliente = bfr.readLine();
+
+                Cliente clienteFactura = BuscadorCliente.buscar(rfcCliente);
+                if(clienteFactura==null) break;
+
+                System.out.println("Generando factura para "+clienteFactura.getNombre()+" "+clienteFactura.getApellido());
+
+                System.out.println("¿Cuántos cargos se harán?");
+                cargos = Integer.parseInt(bfr.readLine());
+                Cargo[] listaCargos = new Cargo[cargos];
+                float total = 0;
+                for (int i = 0; i < cargos; i++) {
+                    listaCargos[i] = new Cargo();
+                    System.out.println("Ingrese el concepto:");
+                    listaCargos[i].setConcepto(bfr.readLine());
+                    System.out.println("Ingrese el precio:");
+                    listaCargos[i].setPrecio(Float.parseFloat(bfr.readLine()));
+                    total += listaCargos[i].getPrecio();
+                    System.out.println("Ingrese la descripción del concepto (Opcional):");
+                    listaCargos[i].setDescripcion(bfr.readLine());
+                }
+                setCliente(clienteFactura);
+                setCargos(listaCargos);
+                setTotal(total);
+
+            }catch (NumberFormatException e){
+                System.out.println("Solo se pueden ingresar números enteros positivos.");
+            } catch (IOException e){
+                System.out.println("Ocurrió un error al leer los datos");
+            }
+        }while(cargos<1);
     }
 
     public void guardar(){
@@ -23,18 +64,42 @@ public class Factura {
             fw.write("---FACTURA---\n");
             fw.write("HOTEL LOS OLIVOS\n\n");
             fw.write(new SimpleDateFormat("dd-MM-yyyy 'a las' HH:mm").format(date)+"\n\n");
-            fw.write("Cliente: "+this.cliente.getNombre()+" "+this.cliente.getApellido()+"\n");
-            fw.write("RFC: "+this.cliente.getRFC()+"\n");
-            fw.write("Método de pago: "+this.cliente.getMetodoPago()+"\n\n");
+            fw.write("Cliente: "+cliente.getNombre()+" "+cliente.getApellido()+"\n");
+            fw.write("RFC: "+cliente.getRFC()+"\n");
+            fw.write("Método de pago: "+cliente.getMetodoPago()+"\n\n");
             fw.write("    Cargo    |    Descripción    |     Precio    \n");
-            for (Cargo cargo : this.cargos) {
+            for (Cargo cargo : cargos) {
                 fw.write(cargo.getConcepto() + "    " + cargo.getDescripcion() + "    $" + cargo.getPrecio() + "\n");
             }
-            fw.write("\nTotal: $"+this.total+" pesos mexicanos (MXN).\n");
+            fw.write("\nTotal: $"+total+" pesos mexicanos (MXN).\n");
             fw.close();
             System.out.println("Factura generada exitosamente.");
         }catch (IOException e){
             System.out.println("Ocurrió un error al generar la factura.");
         }
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cargo[] getCargos() {
+        return cargos;
+    }
+
+    public void setCargos(Cargo[] cargos) {
+        this.cargos = cargos;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
     }
 }
--- src/com/itver/MenuFactura.java
+++ src/com/itver/MenuFactura.java
@@ -13,44 +13,10 @@ public class MenuFactura extends Menu{
 
     @Override
     public void iniciar() {
-        this.leerDatos();
+        Factura factura = new Factura();
+        factura.leer(lector);
+        factura.guardar();
     }
 
-
-    public void leerDatos(){
-        int cargos=0;
-        do{
-            try {
-                System.out.println("Ingrese el RFC del cliente:");
-                String rfcCliente = lector.readLine();
-
-                Cliente clienteFactura = BuscadorCliente.buscar(rfcCliente);
-                if(clienteFactura==null) break;
-
-                System.out.println("Generando factura para "+clienteFactura.getNombre()+" "+clienteFactura.getApellido());
-
-                System.out.println("¿Cuántos cargos se harán?");
-                cargos = Integer.parseInt(lector.readLine());
-                Cargo[] listaCargos = new Cargo[cargos];
-                float total = 0;
-                for (int i = 0; i < cargos; i++) {
-                    listaCargos[i]= new Cargo();
-                    System.out.println("Ingrese el concepto:");
-                    listaCargos[i].setConcepto(lector.readLine());
-                    System.out.println("Ingrese el precio:");
-                    listaCargos[i].setPrecio(Float.parseFloat(lector.readLine()));
-                    total+=listaCargos[i].getPrecio();
-                    System.out.println("Ingrese la descripción del concepto (Opcional):");
-                    listaCargos[i].setDescripcion(lector.readLine());
-                }
-                Factura factura = new Factura(clienteFactura,listaCargos,total);
-                factura.guardar();
-            }catch (NumberFormatException e){
-                System.out.println("Solo se pueden ingresar números enteros positivos.");
-            } catch (IOException e){
-                System.out.println("Ocurrió un error al leer los datos");
-            }
-        }while(cargos<1);
-    }
 
 }
